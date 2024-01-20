3875d1e958bd119ee70617bc16cf27583cd85265
ParkSungCheol <skfo8gmlakd@naver.com>
2024-01-21 07:59:48 +0900

    Git Actions TEST

--- src/main/java/com/example/demo2/testComponent.java
+++ src/main/java/com/example/demo2/testComponent.java
@@ -12,7 +12,7 @@ public class testComponent {
     @GetMapping(value = "/")
     public String test() {
         // 원하는 처리 수행
-        return "[GET] test2 is running@@@@";
+        return "[GET] test2 is running ###";
     }
 
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
